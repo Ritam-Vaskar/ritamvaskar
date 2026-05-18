f6ffc9eadde126736e5ebf2840aac9f9d3612268
Ritam-Vaskar <vaskarananda77@gmail.com>
2026-05-18 21:34:02 +0530

    order chng

--- app/page.js
+++ app/page.js
@@ -12,8 +12,8 @@ export default function Home() {
     <main className="h-full w-full">
       <div className="flex flex-col gap-4 md:gap-6 h-fit">
         <section id="home"><AboutMe /></section>
-        <section id="education"><Education /></section>
         <section id="experience"><Experience /></section>
+        <section id="education"><Education /></section>
         <section id="skills"><SkillsPreview /></section>
         <section id="project"><ProjectsPreview /></section>
         <section id="contact"><Contact /></section>
